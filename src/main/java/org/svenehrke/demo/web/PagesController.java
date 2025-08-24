a55b3bbbf75e3d3b76c432ac8de84c9ce18c73c7
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-08-24 10:34:14 +0200

    page1 and page 2 working

--- src/main/java/org/svenehrke/demo/web/PagesController.java
+++ src/main/java/org/svenehrke/demo/web/PagesController.java
@@ -7,10 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PagesController {
 
 	public static final String PAGE_1_URL = "/ui/pages/page1";
+	public static final String PAGE_2_URL = "/ui/pages/page2";
 
 	@GetMapping(PAGE_1_URL)
 	public String page1() {
 		return "pages/page1";
+	}
+
+	@GetMapping(PAGE_2_URL)
+	public String page2() {
+		return "pages/page2";
 	}
 
 }
